e96c0d967fe36728ec654bdc5b98a087c3e54358
jayasuriyacj <jayasuriyac070@gmail.com>
2017-06-30 14:16:13 +0530

    Create alpt.c

---
+++ alpt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+clrscr();
+printf("\n enter a character");
+scanf("%d",&ch);
+if((ch>='a'&&ch<='z')||(ch>='A"&&ch<='Z'))
+{
+printf("\n alphabet");
+}
+else
+{
+printf("\n not alphabet");
+getch();
+}
+}
+
